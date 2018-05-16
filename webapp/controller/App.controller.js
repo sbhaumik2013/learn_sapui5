5e5b18514b3e9b9c12a7db58f5dc3b0e2e199380
S0014464407 <soujanya.bhaumik@valero.com>
2018-05-16 19:26:57 +0000

    data model and i18n

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"], function (Controller){
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+	], function (Controller, MessageToast){
 		"use strict";
 		return Controller.extend("opensap.myapp.controller.App", {
 		onShowHello : function () {
-			// show a native JavaScript alert
-			//alert("Hello World");
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			
+			MessageToast.show(sMsg);
 			}
 		});
 	});
