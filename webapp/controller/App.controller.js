b4946e95a08bcc98fd245556fa7c3373fd28449f
S0014464407 <soujanya.bhaumik@valero.com>
2018-05-17 18:19:59 +0000

    filtering, sorting and grouping Week 2 Unit 4

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,34 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-	], function (Controller, MessageToast){
+	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+	], function (Controller, MessageToast, Filter, FilterOperator){
 		"use strict";
+		
 		return Controller.extend("opensap.myapp.controller.App", {
-		onShowHello : function () {
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			
-			MessageToast.show(sMsg);
+			onShowHello : function () {
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				
+				MessageToast.show(sMsg);
+			},
+				
+			onFilterProducts : function (oEvent) {
+				// build filter array
+				var aFilter = [], sQuery = oEvent.getParameter("query"),
+					// retrieve list control
+					oList = this.getView().byId("productsList"),
+					// get binding for aggregation 'items'
+					oBinding = oList.getBinding("items");
+				if (sQuery) {
+					aFilter.push(new Filter("ProductID", FilterOperator.Contains, sQuery));
+				}
+				// apply filter. an empty filter array simply removes the filter
+				// which will make all entries visible again
+				oBinding.filter(aFilter);
 			}
 		});
 	});
